384a395227521b753b666c25ccd19655c7814f85
Jirayu Laungwilawan <jirayu.l@ku.th>
2018-11-27 13:53:12 +0700

    Add allowBit and divideBit

--- src/createSubNet.js
+++ src/createSubNet.js
@@ -2,7 +2,7 @@ let mask = ''
 let maximumNumber = 0
 let networkClass = 'No Class'
 let table = []
-let bitDivide = 0
+let divideBit = 0
 let allowBits = 0
 let tableSubnet = []
 
@@ -11,24 +11,26 @@ function createSubNet(ip, networks, hosts) {
   let tempIp = ip.split('.')
   if (!verifyIp(tempIp)) {
     return console.log('This is not ipv4.')
+  } else {
+    networkClass = checkClass(tempIp)
   }
 
-  checkClass(tempIp)
-
   if (networks == 0 && hosts > 0) {
-    bitDivide = findSubnetBits(hosts)
+    divideBit = findSubnetBits(allowBits - hosts)
   } else if (hosts == 0 && networks > 0) {
-    bitDivide = findSubnetBits(networks)
+    divideBit = findSubnetBits(networks)
   }
 
   if (!checkNetworks(networks) || !checkHost(hosts)) {
     return console.log('The number of networks/hosts can not divide the subnet.')
   }
 
-
-
   console.log('IP: ' + ip + '\nMask: ' + mask + '\nMaximum number: ' + maximumNumber + '\nNetwork class: ' + networkClass + '\nNetwork: ' + networks + '\nHosts: ' + hosts)
-  console.log('Bits to diveide: ' + bitDivide)
+  console.log('Bits to diveide: ' + divideBit)
+
+  for (let i = 0; i < divideBit + 1; i++) {
+
+  }
 
 }
 
@@ -39,37 +41,39 @@ function findSubnetBits(numberSubnet) {
 }
 
 function checkNetworks(networks) {
-  return networks >= 0 && networks <= maximumNumber && bitDivide >= allowBits
+  return networks >= 0 && networks <= maximumNumber && divideBit >= allowBits
 }
 
 function checkHost(hosts) {
-  return hosts >= 0 && hosts <= maximumNumber && bitDivide >= allowBits
+  return hosts >= 0 && hosts <= maximumNumber && divideBit >= allowBits
 }
 
 function checkClass(ip) {
   let domain = parseInt(ip[0])
+  let netClass = ''
   if (isClassA(domain)) {
     mask = '255.0.0.0'
     maximumNumber = Math.pow(2, 23)
-    networkClass = 'Class A'
+    netClass = 'Class A'
     allowBits = 24
   } else if (isClassB(domain)) {
     mask = '255.255.0.0'
     maximumNumber = Math.pow(2, 15)
-    networkClass = 'Class B'
+    netClass = 'Class B'
     allowBits = 16
   } else if (isClassC(domain)) {
     mask = '255.255.255.0'
     maximumNumber = Math.pow(2, 7)
-    networkClass = 'Class C'
+    netClass = 'Class C'
     allowBits = 8
   } else if (isClassD(domain)) {
     mask = 'No Mask'
-    networkClass = 'Class D'
+    netClass = 'Class D'
   } else if (isClassE(domain)) {
     mask = 'No Mask'
-    networkClass = 'Class E'
+    netClass = 'Class E'
   }
+  return netClass
 }
 
 function verifyIp(ip) {
